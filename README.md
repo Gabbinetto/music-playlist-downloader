caff05bc8f2e1e71f408816306efbe7a1c9efbbf
GabrieleFerrante <gabrieleferrante97@googlemail.com>
2023-04-01 14:50:45 +0200

    Refined web page

---
+++ README.md
@@ -0,0 +1,29 @@
+# Music playlist downloader
+
+Ever needed to download your YouTube playlist as music? This is the tool you need. Just input the playlist link and download.
+
+## Requirements
+* Python 3.10
+* Flask
+* Pytube
+
+## Usage
+
+* Clone the repository (You can also [download the repository as a zip file](https://github.com/Gabbinetto/music-playlist-downloader/archive/refs/heads/master.zip))
+    ```
+    git clone https://github.com/Gabbinetto/music-playlist-downloader.git
+    ```
+* Navigate to the directory and run the Flask app
+    ```
+    flask run
+    ```
+    You can use this on another device connected to the same network with
+    ```
+    flask run --host=0.0.0.0
+    ```
+* Connect to the ip address shown, which may be `localhost:5000` or other LAN addresses.
+
+You can also host your own server or use hosting services like [PythonAnywhere](https://www.pythonanywhere.com/).
+
+## Credits
+Myself and Pytube and Flask which made this possible.
--- static/redirect.js
+++ static/redirect.js
@@ -1,8 +1,16 @@
-const button = document.getElementById('download-button');
-const urlInput = document.getElementById('url-input')
-button.onclick = () => {
+const button = document.getElementById("download-button");
+const urlInput = document.getElementById("url-input");
+const interface = document.getElementById("interface");
+const thankyou = document.getElementById("thankyou");
 
-    // const id = urlInput.value.replace('https://www.youtube.com/playlist?list=', '')
+
+button.onclick = download
+urlInput.onkeyup = (key) => {if (key == "Enter") download()};
+    
+
+function download() {
+
+    // const id = urlInput.value.replace("https://www.youtube.com/playlist?list=", "")
 
     console.log(urlInput.value)
 
@@ -10,5 +18,12 @@ button.onclick = () => {
         return
     }
 
-    window.location.href = 'download?url=' + encodeURIComponent(urlInput.value)
+    // Hide the interface and show the thank you content
+    interface.style.visibility = "hidden";
+    interface.style.position = "absolute";
+    thankyou.style.visibility = "visible";
+
+
+    // Go to download
+    window.location.href = "download?url=" + encodeURIComponent(urlInput.value)
 }
